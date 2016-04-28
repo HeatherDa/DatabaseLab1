fe3c610505f76b5d5e761cc5a02f4dc85aeeb662
HeatherDa <crystal_tree@hotmail.com>
2016-04-27 20:33:01 -0500

    It works now

--- src/com/Heather/Main.java
+++ src/com/Heather/Main.java
@@ -11,49 +11,72 @@ public class Main {
     static final String PASSWORD = "ashlynn8";
     static Scanner scan=new Scanner(System.in);
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) {
 	// write your code here
         try {
             Class.forName(JDBC_DRIVER);
+
         }catch(ClassNotFoundException cnfe){
             System.out.println("Can't instantiate driver class; check you have drives and classpath configured correctly?");
             cnfe.printStackTrace();
             System.exit(-1);
         }
-        Connection connection = DriverManager.getConnection(DB_CONNECTION_URL, USER, PASSWORD);
-        Statement statement = connection.createStatement();
+        Statement statement=null;
+        Connection connect=null;
 
-        statement.execute("CREATE TABLE IF NOT EXISTS rubikscube (CUBE_SOLVER VARCHAR (50), SOLVING_TIME DOUBLE, )");
-        statement.execute("INSERT INTO rubikscube VALUES ('Cubestormer II robot', 5.270)");
-        statement.execute("INSERT INTO rubikscube VALUES ('Fakhri Raihaan (using his feet)', 27.93)");
-        statement.execute("INSERT INTO rubikscube VALUES ('Ruxin Liu (age 3)', 99.33)");
-        statement.execute("INSERT INTO rubikscube VALUES ('Mats Valk (human record holder)', 6.27)");
+        try {
+            connect = DriverManager.getConnection(DB_CONNECTION_URL, USER, PASSWORD);
+            System.out.println("is there really a connection? "+connect);//yes there is
+            statement = connect.createStatement();
+            System.out.println("is there really a statemnt? "+statement);//yes there is
 
-        ResultSet results=statement.executeQuery("SELECT * FROM rubikscube");
-        while (results.next()){
-            String solver=results.getString("cube_solver");
-            Double time=results.getDouble("solving_time");
-            System.out.println("Solver of Rubik's Cube= "+ solver+ " Time in seconds to solve= "+time);
+            String newTable="CREATE TABLE IF NOT EXISTS rubikscube (Solver varchar (50), Solving_time double)";
+            statement.executeUpdate(newTable);
+            System.out.println("I made a rubikscube table");//doesn't get here
+
+            String addMe="INSERT INTO rubikscube VALUES ('Cubestormer II robot', 5.270)";
+            statement.executeUpdate(addMe);
+            statement.executeUpdate("INSERT INTO rubikscube VALUES ('Fakhri Raihaan (using his feet)', 27.93)");
+            statement.executeUpdate("INSERT INTO rubikscube VALUES ('Ruxin Liu (age 3)', 99.33)");
+            statement.executeUpdate("INSERT INTO rubikscube VALUES ('Mats Valk (human record holder)', 6.27)");
+
+            ResultSet results = statement.executeQuery("SELECT * FROM rubikscube");
+            while (results.next()) {
+                String solver = results.getString("Solver");
+                Double time = results.getDouble("Solving_time");
+                System.out.println("Solver of Rubik's Cube= " + solver + " Time in seconds to solve= " + time);
+            }
+            System.out.println("Do you want to add a new entry?  Type y for yes.");
+            String new_entry = scan.nextLine();
+            String name;
+            Double timing;
+            if (new_entry.equals("y")) {
+                System.out.println("What is the solver's name?");
+                name = scan.nextLine();
+                System.out.println("How many seconds did it take?");
+                timing = scan.nextDouble();//TODO verify correct input
+                statement.execute("INSERT INTO rubikscube VALUES (Solver, Solving_time)");
+            }
+        }catch(SQLException se){
+            se.printStackTrace();
+        }finally{
+            try{
+                if (statement!=null){
+                    statement.close();
+
+                }
+            }catch (SQLException se){
+                se.printStackTrace();
+            }
+            try{
+                if (connect !=null){
+                    connect.close();
+                }
+            }catch(SQLException se){
+                se.printStackTrace();
+            }
         }
-        System.out.println("Do you want to add a new entry?  Type y for yes.");
-        String new_entry=scan.nextLine();
-        String name;
-        Double timing;
-        if (new_entry.equals("y")){
-            System.out.println("What is the solver's name?");
-            name=scan.nextLine();
-            System.out.println("How many seconds did it take?");
-            timing=scan.nextDouble();//TODO verify correct input
-            statement.execute("INSERT INTO rubikscube VALUES (name, timing)");
-        }
-
-
-
-
-
-        results.close();
-        statement.close();
-        connection.close();
+        System.out.println("Program finished");
 
     }
 }
